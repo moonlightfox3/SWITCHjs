5c5c4877ec5e06840f7552a22dc812b420ff6614
JoeCoding3 <tiredredstoner@gmail.com>
2025-01-05 11:06:38 -0800

    modified:   explorer.html 	modified:   html/bcsv.html 	modified:   html/bmg.html 	modified:   html/byml.html 	modified:   html/rarc.html 	modified:   html/sarc.html 	modified:   html/style/menu.css 	modified:   html/style/style.css 	modified:   html/yaz0.html 	modified:   index.html 	new file:   libs/profile.js

---
+++ libs/profile.js
@@ -0,0 +1,16 @@
+let profileExpanded = false
+addEventListener("load", function () {
+    profile = document.createElement("div")
+    profile.id = "profile"
+    profile.innerHTML = `SWITCHjs, made by <i>moonlightfox</i>. (<a class="profileLink" href="https://github.com/JoeCoding3/SWITCHjs"><b>GitHub repo here</b></a>)`
+    profileExpand = document.createElement("div")
+    profileExpand.id = "profileExpand"
+    profileExpand.innerHTML = `<b>i</b>`
+    document.body.append(profile, profileExpand)
+
+    profileExpand.onclick = function () {
+        profileExpanded = !profileExpanded
+        if (profileExpanded) profile.style.display = "block"
+        else profile.style.display = "none"
+    }
+})
