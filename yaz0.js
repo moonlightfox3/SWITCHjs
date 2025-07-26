99d2ad7eaa7c42e9cd22da6b0d3c6cb0465dbe07
JoeCoding3 <tiredredstoner@gmail.com>
2025-07-26 00:24:40 -0700

    modified:   html/yaz0.html 	modified:   index.html 	modified:   yaz0.js

--- yaz0.js
+++ yaz0.js
@@ -1,6 +1,6 @@
 let resultYaz0 = null
 let resultNameYaz0 = null
-let inFileTypesYaz0 = ["szs", "arc", "sesetlist", "sbmaptex", "sbmapopen", "sbitemico", "sbstftex"]
+let inFileTypesYaz0 = ["szs", "arc", "sbfarc", "sesetlist", "sbmaptex", "sbmapopen", "sbitemico", "sbstftex", "sbreviewtex", "sstats"]
 let outFileTypeYaz0 = "bin"
 async function decompressFileFromYaz0 () {
     let file = await importFile(inFileTypesYaz0)
