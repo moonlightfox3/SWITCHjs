725147c2d31d2c4261f5008eb23bbf8015fb5492
JoeCoding3 <tiredredstoner@gmail.com>
2025-07-30 13:46:32 -0700

    modified:   libs/files.js

--- libs/files.js
+++ libs/files.js
@@ -182,6 +182,16 @@ function getFileType (fileBuf, fileExt) {
         name = `BMG_${mode}`
         ext = "bmg"
     }
+
+    else if (fileBuf.str(0x00, 0x04) == "ARSL") {
+        let mode = null
+        let byteOrder = fileBuf.int(0x04, IntSize.U16, Endian.BIG)
+            byteOrder = byteOrder.toString(16)
+            if (byteOrder == "feff") mode = Endian.BIG
+            else if (byteOrder == "fffe") mode = Endian.LITTLE
+        name = `BARSLIST_${mode}`
+        ext = "barslist"
+    }
     
     else {
         name = "?"
